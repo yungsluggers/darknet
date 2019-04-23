f89c2d54862cb84b7ba7cd33844393bba2e4bc8a
kevinaltschuler <kevinaltschuler@gmail.com>
2019-04-23 14:09:54 -0400

    made it a loop

--- examples/classifier.c
+++ examples/classifier.c
@@ -114,88 +114,100 @@ void predict_classifier(char *datacfg, char *cfgfile, char *weightfile, char *la
 
 void one_label_classifier(char *datacfg, char *cfgfile, char *weightfile, char *label, int top, char *filepath, char *size)
 {
-    int sizeInt = atoi(size);
-    //printf("value of imagedata: %.*s\n", (int)sizeof(imagedata) + 7, imagedata);
-    // converting string ex: 123,242,234,234 to int array
-    int totalsize = sizeInt * sizeInt * 4;
-    int imgIntArray[90000] = {};
-
-    char imagedata[999999];
-    FILE *f = fopen(filepath, "r");
-    fscanf(f, "%[^\n]", imagedata);
-    fclose(f);
-
-    //memset(imgIntArray, 0, totalsize * sizeof(int));
-    char *tok = strtok(imagedata, ",");
-    int i = 0;
-    // Keep going until we run out of tokens
-    while (tok)
-    {
-        //printf(tok);
-        //printf("\n");
-        // Don't overflow your target array
-        if (i < totalsize)
-        {
-            // Convert to integer and store it
-            imgIntArray[i++] = atoi(tok);
-        }
-        // Get the next token from the string - note the use of NULL
-        // instead of the string in this case - that tells it to carry
-        // on from where it left off.
-        tok = strtok(NULL, ",");
-    }
-
     network *net = load_network(cfgfile, weightfile, 0);
     set_batch_network(net, 1);
     srand(2222222);
 
-    //list *options = read_data_cfg(datacfg);
+    char buff[256];
+    char *input = buff;
 
-    char *name_list = "data/9k.labels";
-    //if(top == 0) top = option_find_int(options, "top", 1);
+    int imgIntArray[90000] = {};
 
-    //printf( "value of namelist: %s\n", name_list );
+    char imagedata[999999];
 
-    char **n = get_labels(name_list);
-
-    i = 0;
-    char *blah = label;
-    while (*n != NULL)
+    while (1)
     {
-        //printf("value of n: %.*s\n", (int)sizeof(*n), *n);
-        if (!strcmp(*n, blah))
+        if (filepath)
         {
-            //printf("found n: %.*s\n", (int)sizeof(*n) + 1, *n);
-            break;
+            strncpy(input, filepath, 256);
         }
-        i++;
-        *n++;
+        else
+        {
+            printf("Enter Image Path: ");
+            fflush(stdout);
+            input = fgets(input, 256, stdin);
+            if (!input)
+                return;
+            strtok(input, "\n");
+        }
+
+        int sizeInt = atoi(size);
+        //printf("value of imagedata: %.*s\n", (int)sizeof(imagedata) + 7, imagedata);
+        // converting string ex: 123,242,234,234 to int array
+        int totalsize = sizeInt * sizeInt * 4;
+
+        FILE *f = fopen(input, "r");
+        fscanf(f, "%[^\n]", imagedata);
+        fclose(f);
+
+        char *tok = strtok(imagedata, ",");
+        int i = 0;
+        // Keep going until we run out of tokens
+        while (tok)
+        {
+            // Don't overflow your target array
+            if (i < totalsize)
+            {
+                // Convert to integer and store it
+                imgIntArray[i++] = atoi(tok);
+            }
+            // Get the next token from the string - note the use of NULL
+            // instead of the string in this case - that tells it to carry
+            // on from where it left off.
+            tok = strtok(NULL, ",");
+        }
+
+        //printf( "value of namelist: %s\n", name_list );
+
+        char **n = get_labels("data/9k.labels");
+
+        i = 0;
+        char *blah = label;
+        while (*n != NULL)
+        {
+            //printf("value of n: %.*s\n", (int)sizeof(*n), *n);
+            if (!strcmp(*n, blah))
+            {
+                //printf("found n: %.*s\n", (int)sizeof(*n) + 1, *n);
+                break;
+            }
+            i++;
+            *n++;
+        }
+
+        int *indexes = calloc(top, sizeof(int));
+        image im = load_image_bitmap(imgIntArray, sizeInt, sizeInt);
+
+        // for (int i=0; i < 99; i++) {
+        //     printf("%lf\n",im.data[i]);
+        // }
+        image r = letterbox_image(im, net->w, net->h);
+        //image r = resize_min(im, 320);
+        //printf("%d %d\n", r.w, r.h);
+        resize_network(net, r.w, r.h);
+        //printf("%d %d\n", r.w, r.h);
+        float *X = r.data;
+        float *predictions = network_predict(net, X);
+        if (net->hierarchy)
+            hierarchy_predictions(predictions, net->outputs, net->hierarchy, 1, 1);
+        top_k(predictions, net->outputs, top, indexes);
+        //fprintf(stderr, "%s: Predicted in %f seconds.\n", input, sec(clock()-time));
+        printf("%.8f", predictions[i]);
+        if (r.data != im.data)
+            free_image(r);
+        free_image(im);
+        /*if (filename) break;*/
     }
-
-    clock_t time;
-    int *indexes = calloc(top, sizeof(int));
-    image im = load_image_bitmap(imgIntArray, sizeInt, sizeInt);
-
-    // for (int i=0; i < 99; i++) {
-    //     printf("%lf\n",im.data[i]);
-    // }
-    image r = letterbox_image(im, net->w, net->h);
-    //image r = resize_min(im, 320);
-    //printf("%d %d\n", r.w, r.h);
-    resize_network(net, r.w, r.h);
-    //printf("%d %d\n", r.w, r.h);
-    float *X = r.data;
-    time = clock();
-    float *predictions = network_predict(net, X);
-    if (net->hierarchy)
-        hierarchy_predictions(predictions, net->outputs, net->hierarchy, 1, 1);
-    top_k(predictions, net->outputs, top, indexes);
-    //fprintf(stderr, "%s: Predicted in %f seconds.\n", input, sec(clock()-time));
-    printf("%.8f", predictions[i]);
-    if (r.data != im.data)
-        free_image(r);
-    free_image(im);
-    //if (filename) break;*/
 }
 
 void run_classifier(int argc, char **argv)
